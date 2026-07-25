5f3852b51b62312f1cd8095922b14a6f70fb1073
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2026-07-25 13:30:20 +0200

    Revert "refactor(cli): run one-shot commands without Pal.Run"
    This reverts commit 88eb8b2a04368062d8130d93c589d62e6397618c.

--- cmd/pal/internal/cli/subscommands/init/init.go
+++ cmd/pal/internal/cli/subscommands/init/init.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/urfave/cli/v3"
+	"github.com/zhulik/pal"
+	"github.com/zhulik/pal/cmd/pal/internal/cli/app"
 )
 
 // New returns the init subcommand.
@@ -14,7 +16,7 @@ func New() *cli.Command {
 		Flags:     Flags(),
 		Arguments: Arguments(),
 		Action: func(ctx context.Context, cmd *cli.Command) error {
-			return (&runner{opts: OptionsFromCommand(cmd)}).Run(ctx)
+			return app.Run(ctx, pal.Provide(&runner{opts: OptionsFromCommand(cmd)}))
 		},
 	}
 }
